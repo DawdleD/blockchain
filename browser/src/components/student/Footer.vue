8e89a3d6c285fe84ed8df734e195157306c11604
MemoryDewey <38499618+MemoryDewey@users.noreply.github.com>
2019-03-31 12:15:47 +0800

    init

--- browser/src/components/student/Footer.vue
+++ browser/src/components/student/Footer.vue
@@ -9,5 +9,16 @@
 </script>
 
 <style scoped>
-
+    .footer {
+        padding-top: 40px;
+        height: 180px;
+        box-sizing:border-box;
+        text-align: center;
+        position: absolute;
+        bottom: 0;
+        width:100%;
+        left: 0;
+        background: #444547;
+        font-size: 12px;
+    }
 </style>
